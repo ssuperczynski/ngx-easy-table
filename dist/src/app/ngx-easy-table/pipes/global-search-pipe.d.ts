8a5c6f68c74ae19386f603aaebf38f13d733584a
Roland Peelen <rpeelen@conclusion.nl>
2018-04-03 14:47:51 +0200

    fixed translations and placeholder

--- dist/src/app/ngx-easy-table/pipes/global-search-pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from '@angular/core';
-import { ResourceService } from '../services/resource-service';
-export declare class GlobalSearchPipe implements PipeTransform {
-    resource: ResourceService;
-    constructor(resource: ResourceService);
-    transform(dataArr: any, filter: any): any;
-}
--- src/app/ngx-easy-table/components/header/header.component.ts
+++ src/app/ngx-easy-table/components/header/header.component.ts
@@ -4,14 +4,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'table-header',
   template: `
     <label for="search_{{ column['key'] }}">
-      <input type="text"
-             id="search_{{ column['key'] }}"
-             aria-label="Search"
-             placeholder="Search {{ column['title'] }}"
-             class="ngx-table__header-search ngx-form-input ngx-input-sm"
-             #input
-             (input)="update.emit({value: input.value, key: column['key']})"
-      >
+      <div class="input-group mb-3  pt-3">
+        <span class="input-group-addon"><i _ngcontent-c3="" class="fa fa-search"></i></span>
+        <input type="text"
+               id="search_{{ column['key'] }}"
+               aria-label="Zoek"
+               placeholder="Zoek: {{ column['title'] }}"
+               class="ngx-table__header-search ngx-form-input ngx-input-sm form-control"
+               #input
+               (input)="update.emit({value: input.value, key: column['key']})">
+      </div>
     </label>`
 })
 
