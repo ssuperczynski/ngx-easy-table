f10fa1301e0fc49cbefd337eb085980060d7d871
Sebastian <s.superczynski@gmail.com>
2022-12-22 09:20:49 +0100

    fixes

--- src/app/demo/dynamic-row/dynamic-row.component.ts
+++ src/app/demo/dynamic-row/dynamic-row.component.ts
@@ -8,7 +8,7 @@ import { Columns, Config, DefaultConfig } from 'ngx-easy-table';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DynamicRowComponent implements OnInit {
-  @ViewChild('actionTpl') actionTpl: TemplateRef<any>;
+  @ViewChild('actionTpl', { static: true }) actionTpl: TemplateRef<any>;
   public data;
   public columns: Columns[];
   public configuration: Config;
